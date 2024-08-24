9dd0cc2c36f1cb3c048f27518bd74abe790f75e5
shizukani9 <tatianamayorga9999@gmail.com>
2024-08-23 21:09:44 -0400

    login

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -3,13 +3,12 @@ const axios = require('axios');
 const environment = require("../../environment.json");
 const configuration = require("../../configuration.json");
 const DriverFactory = require("../../core/ui/driverFactory");
-const LoginPage = require("../../main/ui/login_page");
+const CustomerloginPage = require("../../main/ui/customer_login_page");
 const CreateAnAccountPage = require('../../main/ui/create_an_account_page');
 const MyAccountCustomer = require("../../main/ui/my_account_customer_page");
 const { until, Key } = require("selenium-webdriver");
 var {setDefaultTimeout} = require('@cucumber/cucumber');
 const UserService = require('../../main/api/userService');
-
 
 setDefaultTimeout(60 * 1000);
 let loginHook = false;
@@ -26,7 +25,6 @@ BeforeAll( { tags: "@ui" }, async function(){
 
 Before({ tags: "@createAccount" }, async function() {
     console.log("Starting Create Account Process");
-    
     const firstNameInput = await DriverFactory.myDriver.wait(until.elementLocated(CreateAnAccountPage.firstNameInput));
     const lastNameInput = await DriverFactory.myDriver.wait(until.elementLocated(CreateAnAccountPage.lastNameInput));
     const emailInput = await DriverFactory.myDriver.wait(until.elementLocated(CreateAnAccountPage.emailInput));
@@ -39,65 +37,35 @@ Before({ tags: "@createAccount" }, async function() {
     await emailInput.sendKeys(environment.demo.user.email);
     await passwordInput.sendKeys(environment.demo.user.password);
     await confirmPasswordInput.sendKeys(environment.demo.user.confirmPassword);
-
     await createAccountButton.click();
 });
 
 After({ tags: "@deleteAccount" }, async function() {
     console.log("Starting Account Deletion Process");
-
     const userService = new UserService();
     const email = environment.demo.user.email;
-
-    try {
-        const userId = await userService.getUserIdByEmail(email);
-        if (userId) {
-            await userService.deleteUserById(userId);
-            console.log(`User with email ${email} deleted successfully.`);
-        } else {
-            console.log(`User with email ${email} not found.`);
-        }
-    } catch (error) {
-        console.error("Account deletion process failed.", error);
+    const userId = await userService.getUserIdByEmail(email);
+    if (userId) {
+        await userService.deleteUserById(userId);
+        console.log(`User with email ${email} deleted successfully.`);
+    } else {
+        console.log(`User with email ${email} not found.`);
     }
 });
-
-
-
-
-
 
 Before( { tags: "@login" }, async function(scenario){
     console.log("Test scenario: " + scenario.pickle.name);
     if ((loginHook === undefined) || (loginHook === false)){
         console.log("Hook: Starting Login");
-        const usernameInput = await DriverFactory.myDriver.wait(until.elementLocated(LoginPage.usernameInput));
-        const nextButton = await DriverFactory.myDriver.wait(until.elementLocated(LoginPage.nextButton));
-        await usernameInput.sendKeys(environment.prod.userAdmin.username);
-        await nextButton.click();
-
-        const passwordInput = await DriverFactory.myDriver.wait(until.elementLocated(LoginPage.passwordInput));
-        await passwordInput.sendKeys(environment.prod.userAdmin.password);
-
-        if (!isCookieEnabled){
-            try {
-                const cookiesButton = await DriverFactory.myDriver.wait(until.elementLocated(LoginPage.cookiesButton));
-                    await cookiesButton.click();
-                    isCookieEnabled = true;
-                } catch (error) {
-                    console.log("No se encontró la ventana emergente de cookies o ya fue cerrada."+error);
-                }
-        }
-        const loginButton = await DriverFactory.myDriver.wait(until.elementLocated(LoginPage.loginButton));
-        await loginButton.click();
+        const emailInput = await DriverFactory.myDriver.wait(until.elementLocated(CustomerloginPage.emailInput));
+        const passwordInput = await DriverFactory.myDriver.wait(until.elementLocated(CustomerloginPage.passwordInput));
+        const signInButton = await DriverFactory.myDriver.wait(until.elementLocated(CustomerloginPage.signInButton));
+        await emailInput.sendKeys(environment.demo.user.email);
+        await passwordInput.sendKeys(environment.demo.user.password);
+        await signInButton.click();
     }
     loginHook = true;
 });
-
-
-
-
-
 
 AfterAll({ tags: "@ui" },async function(){
     //await DriverFactory.closeDriver();
