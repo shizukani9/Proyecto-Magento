1cacfbe42bd8f4f3a8195ef3e1a24266be69a33e
shizukani9 <tatianamayorga9999@gmail.com>
2024-09-08 22:30:00 -0400

    test cases 1 direccion de envio

--- features/direccion_envio.feature
+++ features/direccion_envio.feature
@@ -1,7 +1,7 @@
 @ui
 Feature: Direccion de envio 
 
-@createAccount @funcional_Positivo @Iteracion1 
+@createAccount @deleteAccount @funcional_Positivo @DE-1 @Iteracion2 @pass
 Scenario: DE-1: Verificar que cuando se ingresa un Nombre (First Name) con 1 carácter entonces la dirección se guarda correctamente.
     Given El usuario está en la página de inicio
         And El usuario navega a página de Yoga Collection
@@ -12,7 +12,7 @@ Scenario: DE-1: Verificar que cuando se ingresa un Nombre (First Name) con 1 car
         And El usuario guarda la dirección
         And La dirección debe guardarse correctamente sin errores
 
-@createAccount @funcional_Positivo @Iteracion1 
+@createAccount @deleteAccount @funcional_Positivo @DE-2 @Iteracion2 @pass
 Scenario: DE-2: Verificar que cuando se ingresa un Nombre (First Name) con 255 caracteres entonces la dirección se guarda correctamente.
     Given El usuario está en la página de inicio
         And El usuario navega a página de Yoga Collection
@@ -22,3 +22,45 @@ Scenario: DE-2: Verificar que cuando se ingresa un Nombre (First Name) con 255 c
     Then El usuario ingresa el nombre con 255 caracteres
         And El usuario guarda la dirección
         And La dirección debe guardarse correctamente sin errores
+
+@createAccount @deleteAccount @funcional_Positivo @DE-3 @Iteracion2 @fail
+Scenario: DE-3: Verificar que cuando se ingresa un Nombre (First Name) con más de 255 caracteres entonces se muestra un mensaje de error.
+    Given El usuario está en la página de inicio
+        And El usuario navega a página de Yoga Collection
+    When El usuario añade un producto al carrito
+    Then La cantidad de productos en el carrito debe actualizarse en la pagina Checkout cart
+    Then El usuario procede a confirmar el producto para la compra
+    Then El usuario ingresa el nombre con mas 255 caracteres
+        And El usuario guarda la dirección
+        And Se muestra un mensaje de error
+
+@createAccount @deleteAccount @funcional_Positivo @DE-4 @Iteracion2 @fail
+Scenario: DE-4: Verificar que cuando se ingresa un Nombre (First Name) vacío entonces se muestra un mensaje de error.
+    Given El usuario está en la página de inicio
+        And El usuario navega a página de Yoga Collection
+    When El usuario añade un producto al carrito
+    Then La cantidad de productos en el carrito debe actualizarse en la pagina Checkout cart
+    Then El usuario procede a confirmar el producto para la compra
+    Then El usuario ingresa el nombre vacio
+        And El usuario guarda la dirección
+        And Se muestra un mensaje de error
+
+@createAccount @deleteAccount @funcional_Positivo @DE-5 @Iteracion2 @fail
+Scenario: DE-5: Verificar que cuando se ingresa un Nombre (First Name) con números entonces se muestra un mensaje de error.
+    Given El usuario está en la página de inicio
+        And El usuario navega a página de Yoga Collection
+    When El usuario añade un producto al carrito
+    Then La cantidad de productos en el carrito debe actualizarse en la pagina Checkout cart
+    Then El usuario ingresa el nombre con numeros
+        And El usuario guarda la dirección
+        And Se muestra un mensaje de error
+
+@createAccount @deleteAccount @funcional_Positivo @DE-6 @Iteracion2 @fail
+Scenario: DE-6: Verificar que cuando se ingresa un Nombre (First Name) con caracteres alfanuméricos entonces se muestra un mensaje de error.
+    Given El usuario está en la página de inicio
+        And El usuario navega a página de Yoga Collection
+    When El usuario añade un producto al carrito
+    Then La cantidad de productos en el carrito debe actualizarse en la pagina Checkout cart
+    Then El usuario ingresa el nombre con alfanumericos
+        And El usuario guarda la dirección
+        And Se muestra un mensaje de error
--- features/support/direccion_envio_steps.js
+++ features/support/direccion_envio_steps.js
@@ -15,7 +15,27 @@ const { until, By, Select } = require('selenium-webdriver');
 
 Then('El usuario ingresa el nombre con solo 1 carácter', async function () {
     console.log("El usuario ingresa el nombre con solo 1 carácter");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
 
+    const firstName = DataGenerator.generateLetters(1);
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
 });
 
 Then('La dirección debe guardarse correctamente sin errores', async function () {
@@ -23,8 +43,132 @@ Then('La dirección debe guardarse correctamente sin errores', async function ()
     
 });
 
-Then('El usuario ingresa el nombre con 255 caracteres', async function () {
-    console.log("El usuario ingresa el nombre con 255 caracteres");
+Then('Se muestra un mensaje de error', async function () {
+    console.log("Se muestra un mensaje de error");
     
 });
 
+Then('El usuario ingresa el nombre con 255 caracteres', async function () {
+    console.log("El usuario ingresa el nombre con 255 caracteres");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
+
+    const firstName = DataGenerator.generateLetters(255);
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
+});
+
+Then('El usuario ingresa el nombre con mas 255 caracteres', async function () {
+    console.log("El usuario ingresa el nombre con mas 255 caracteres");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
+
+    const firstName = DataGenerator.generateLetters(300);
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
+});
+
+Then('El usuario ingresa el nombre vacio', async function () {
+    console.log("El usuario ingresa el nombre vacio");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
+
+    const firstName = "";
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
+});
+
+Then('El usuario ingresa el nombre con numeros', async function () {
+    console.log("El usuario ingresa el nombre con numeros");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
+
+    const firstName = DataGenerator.generateRandomNumber(10);
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
+});
+
+Then('El usuario ingresa el nombre con alfanumericos', async function () {
+    console.log("El usuario ingresa el nombre con alfanumericos");
+    await DriverFactory.myDriver.sleep(4000);
+    await DriverFactory.myDriver.wait(until.urlContains('https://magento2-demo.magebit.com/checkout/#shipping'), configuration.browser.timeout);
+
+    const firstName = DataGenerator.generateAlphanumeric(10);
+    const lastName = environment.demo.userShippingAddress.lastName;
+    const company = environment.demo.userShippingAddress.company;
+    const streetAddress = environment.demo.userShippingAddress.streetAddress;
+    const stateValue = "2";
+    const city = environment.demo.userShippingAddress.city;
+    const postcode = environment.demo.userShippingAddress.zipPostal;
+    const phoneNumber = environment.demo.userShippingAddress.phoneNumber;
+
+    await ShippingAdressPage.enterFirstName(firstName);
+    await ShippingAdressPage.enterLastName(lastName);
+    await ShippingAdressPage.enterCompany(company);
+    await ShippingAdressPage.enterStreetAddress(streetAddress);
+    await ShippingAdressPage.selectState(stateValue);
+    await ShippingAdressPage.enterCity(city);
+    await ShippingAdressPage.enterPostcode(postcode);
+    await ShippingAdressPage.enterTelephone(phoneNumber);
+    await ShippingAdressPage.selectShippingMethod();
+});
