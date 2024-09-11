9eb92a4cf0ead52cc2aede9fb4e99eec08084192
shizukani9 <tatianamayorga9999@gmail.com>
2024-09-11 04:09:13 -0400

    test

---
+++ features/support/confirmacion_pedido_steps.js
@@ -0,0 +1,14 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+const DriverFactory = require("../../core/ui/driverFactory");
+let chai = require('chai');
+let expect = chai.expect;
+const YogaPage = require('../../main/ui/yoga_page');
+const HomePage = require('../../main/ui/home_page');
+const CheckoutCarPage = require('../../main/ui/checkout_car_page');
+const ShippingAdressPage = require('../../main/ui/shipping_address_page');
+const ReviewAndPaymentsPage = require('../../main/ui/review_and_payments_page');
+const CheckoutSucessPage = require('../../main/ui/checkout_sucess_page');
+const DataGenerator = require('../../utils/dataGenerator');
+const environment = require("../../environment.json");
+const configuration = require("../../configuration.json");
+const { until, By, Select } = require('selenium-webdriver');
